7ef38dd78c7733aa46a4a44e6437e6c5bf70ef0b
Anton Krylov <anthony.kryloff@gmail.com>
2019-03-23 14:38:29 +0300

    init Spring Boot application

---
+++ src/main/java/me/aekrylov/technaxis_test/TechnaxisTestApplication.java
@@ -0,0 +1,13 @@
+package me.aekrylov.technaxis_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnaxisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnaxisTestApplication.class, args);
+    }
+
+}
